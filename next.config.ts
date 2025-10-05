df9a33bf6787b5493a438d90f5b73a9a170a58f5
3mindedscholar <10akhil.t@gmail.com>
2025-10-05 15:48:33 +0530

    Fixed qr gen issues

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["picsum.photos"],
+    domains: [
+      "picsum.photos",
+      "api.qrserver.com"
+    ],
   },
 };
 
